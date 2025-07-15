4de495c44bf8d88751a2e1b08fb3b1c0a5b2b8ed
Oleksandr-Marych <112111959+AlexMarych@users.noreply.github.com>
2025-07-15 15:06:15 +0200

    XOR anotation deleted

--- Pet_project/src/main/java/org/spring/pet_project/Controller/DTO/Request/WriteMessageDto.java
+++ Pet_project/src/main/java/org/spring/pet_project/Controller/DTO/Request/WriteMessageDto.java
@@ -3,11 +3,9 @@ package org.spring.pet_project.Controller.DTO.Request;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import org.spring.pet_project.Config.XOR;
 
 import java.util.UUID;
 
-@XOR(fields = { "taskId", "chatId" })
 public record WriteMessageDto(
         @NotBlank
         @Min(1)
