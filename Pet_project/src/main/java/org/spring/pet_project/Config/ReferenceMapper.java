21673c52569581d628649fd638151c57c8ddbf19
Oleksandr-Marych <112111959+AlexMarych@users.noreply.github.com>
2025-07-15 21:13:34 +0200

    Methods for set mapping

--- Pet_project/src/main/java/org/spring/pet_project/Config/ReferenceMapper.java
+++ Pet_project/src/main/java/org/spring/pet_project/Config/ReferenceMapper.java
@@ -7,7 +7,9 @@ import org.mapstruct.ObjectFactory;
 import org.mapstruct.TargetType;
 import org.springframework.stereotype.Component;
 
+import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Component
 public class ReferenceMapper {
@@ -17,5 +19,11 @@ public class ReferenceMapper {
     @ObjectFactory
     public <T> T map(@NonNull final UUID id, @TargetType Class<T> type) {
         return entityManager.getReference(type, id);
+    }
+
+    public <T> Set<T> mapSet(@NonNull Set<UUID> ids, @TargetType Class<T> type) {
+        return ids.stream()
+                .map(id -> entityManager.getReference(type, id))
+                .collect(Collectors.toSet());
     }
 }
