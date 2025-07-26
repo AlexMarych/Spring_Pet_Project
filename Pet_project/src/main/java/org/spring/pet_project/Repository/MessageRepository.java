11d572e96f31c33324c60615ae38155191319c3d
Oleksandr-Marych <112111959+AlexMarych@users.noreply.github.com>
2025-07-26 23:30:41 +0200

    Repositories method added
    method for retriving lists of enities by one of the relation attribute

--- Pet_project/src/main/java/org/spring/pet_project/Repository/MessageRepository.java
+++ Pet_project/src/main/java/org/spring/pet_project/Repository/MessageRepository.java
@@ -1,11 +1,18 @@
 package org.spring.pet_project.Repository;
 
-import org.spring.pet_project.Model.Message;
+import org.spring.pet_project.Model.Messages.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface MessageRepository extends JpaRepository<Message, UUID> {
+public interface MessageRepository<T extends Message> extends JpaRepository<T, UUID> {
+
+    List<T> findAllByChatId(UUID chatId);
+
+    List<T> findAllByTaskId(UUID taskId);
+
+
 }
--- Pet_project/src/main/java/org/spring/pet_project/Repository/TaskListRepository.java
+++ Pet_project/src/main/java/org/spring/pet_project/Repository/TaskListRepository.java
@@ -4,8 +4,11 @@ import org.spring.pet_project.Model.TaskList;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface TaskListRepository extends JpaRepository<TaskList, UUID> {
+
+    List<TaskList> findAllByBoardId(UUID boardId);
 }
